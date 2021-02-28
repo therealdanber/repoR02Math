6c4e8e241461d0dbe0d21597fb6451d5884194fc
therealdanber <Daniel_Berk@gmx.de>
2021-02-28 01:24:47 +0100

    1/loc/db: Math01.R: Eq. of straight line + plot

--- Math01.R
+++ Math01.R
@@ -14,6 +14,14 @@ library(magrittr) # %<>%
 #Straight line equation: slope-intercept form
 m <- 1 # m: slope
 c <- 0 # c: y-intercept
-x <- 0
-y <- m*x + c 
-m; x; c; y
+#x <- 0
+#y <- m*x + c 
+#m; x; c; y
+x <- c(0:5); x; length(x)
+y <- c(0:5)*0; y
+for (i in 1:length(x)){
+  y[i] <- m*x[i] + c
+  print(paste("x=",x[i]," y=",y[i]))
+}
+print(x); y
+plot(x,y)
